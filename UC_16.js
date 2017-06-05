c9b5ba71dd0f3204f02c31c2ed77d6ebad0bf943
Chloe Thies <ChloeThies@Chloes-MacBook.local>
2017-06-05 10:52:52 -0700

    minor UI changes

--- UC_16.js
+++ UC_16.js
@@ -1,16 +1,18 @@
 function findQuestion(questionId) {
-      // Now make a HTTP request
+    // Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
-            if(this.status === 200) {
+            if (this.status === 200) {
                 // The request was successful!
                 displayQuestion(this.responseText);
-            } else {
+            }
+            else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
-        } else {
+        }
+        else {
             // Waiting for a response...
         }
     }
@@ -19,34 +21,35 @@ function findQuestion(questionId) {
     httpRequest.send();
     console.log("success");
 }
-
 var response;
 
 function displayQuestion(data) {
-response = JSON.parse(data);
+    response = JSON.parse(data);
     console.log(response);
     console.log(response.results[0].question);
-    document.getElementById("response").className = "alert alert-success";
+    document.getElementById("response").className = "alert alert-info";
     document.getElementById("response").innerHTML = response.results[0]["question"];
     document.getElementById("answer").className = null;
     document.getElementById("answer").innerHTML = "";
 }
 
 function responseTrue() {
-    if(response.results[0]["correct_answer"] == "True") {
+    if (response.results[0]["correct_answer"] == "True") {
         document.getElementById("answer").className = "alert alert-success";
         document.getElementById("answer").innerHTML = "CORRECT!"
-    } else {
-        document.getElementById("answer").className = "alert alert-warning";
+    }
+    else {
+        document.getElementById("answer").className = "alert alert-danger";
         document.getElementById("answer").innerHTML = "The correct answer is " + response.results[0]["correct_answer"];
     }
 }
 
 function responseFalse() {
-    if(response.results[0]["correct_answer"] == "False") {
+    if (response.results[0]["correct_answer"] == "False") {
         document.getElementById("answer").className = "alert alert-success";
         document.getElementById("answer").innerHTML = "CORRECT!"
-    } else {
+    }
+    else {
         document.getElementById("answer").className = "alert alert-warning";
         document.getElementById("answer").innerHTML = "The correct answer is " + response.results[0]["correct_answer"];
     }
